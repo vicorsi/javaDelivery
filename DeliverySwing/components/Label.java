e525d27aaf92fa1813e0a230858eb3e8179a9092
Victor Corsi <126412605+vicorsi@users.noreply.github.com>
2023-10-17 10:40:48 -0300

    Delete DeliverySwing directory

--- DeliverySwing/components/Label.java
+++
@@ -1,9 +0,0 @@
-package components;
-
-import javax.swing.*;
-
-public class Label extends JLabel {
-    public Label(int x, int y, int width, int height){
-        setBounds(x, y, width, height);
-    }
-}
--- DeliverySwing/components/Passwordfield.java
+++
@@ -1,9 +0,0 @@
-package components;
-
-import javax.swing.*;
-
-public class Passwordfield extends JPasswordField {
-    public Passwordfield(int x, int y, int width, int heihgt){
-        setBounds(x, y, width, heihgt);
-    }
-}
